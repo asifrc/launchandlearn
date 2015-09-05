4858fc9d2812bcc697f8c8c9a308a25e0c510387
Asif R Choudhury <choudhury.asif@gmail.com>
2015-09-05 00:41:22 -0500

    Added describe stack endpoint

--- routes/stacks.js
+++ routes/stacks.js
@@ -53,7 +53,22 @@ var deleteStack = function(req, res) {
   });
 };
 
+var describeStack = function(req, res) {
+  var params = {
+    "StackName": stackName,
+  };
+  cloudformation.describeStacks(params, function(err, data) {
+    if (err) {
+      respond(res.status(200), "DOES_NOT_EXIST");
+    }
+    else {
+      respond(res.status(200), data["Stacks"][0]["StackStatus"]);
+    }
+  });
+};
+
 router.post('/', createStack);
 router.delete('/', deleteStack);
+router.get('/', describeStack);
 
 module.exports = router;
