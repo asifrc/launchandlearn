b1112df3e6aed934f1f8f4a8dda780c234a10895
Asif R Choudhury <choudhury.asif@gmail.com>
2015-09-05 00:41:37 -0500

    Poll for status change for creation and deletion (get info button obsolete)

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,38 @@
+var POLL_TIMEOUT = 10000;
+
 var displayStatus = function(message) {
   $('#dvStatus').html(message);
+};
+
+var stackCreated = function(status) {
+  displayStatus("Stack Created! " + status);
+};
+
+var stackDeleted = function(status) {
+  displayStatus("Stack Deleted! " + status);
+};
+
+
+var pollForCreation = function() {
+  $.get('/stacks', function(status) {
+    if (status === "CREATE_IN_PROGRESS") {
+      setTimeout(pollForCreation, POLL_TIMEOUT);
+    }
+    else {
+      stackCreated(status);
+    }
+  });
+};
+
+var pollForDeletion = function() {
+  $.get('/stacks', function(status) {
+    if (status === "DOES_NOT_EXIST") {
+      stackDeleted(status);
+    }
+    else {
+      setTimeout(pollForDeletion, POLL_TIMEOUT);
+    }
+  });
 };
 
 var createStack = function() {
@@ -7,12 +40,11 @@ var createStack = function() {
   $request = $.post('/stacks');
   $request.done(function(data) {
     asif = data;
-    console.log(data);
     displayStatus("Creating New VM...");
+    pollForCreation();
   });
   $request.fail(function(error) {
     asif = error;
-    console.log(error);
    displayStatus("Fail :(" + error.responseText);
   });
 };
@@ -22,12 +54,24 @@ var deleteStack = function() {
   $request = $.ajax({url: '/stacks', type: 'DELETE'});
   $request.done(function(data) {
     asif = data;
-    console.log(data);
     displayStatus("Deleting VM...");
+    pollForDeletion();
   });
   $request.fail(function(error) {
     asif = error;
-    console.log(error);
+   displayStatus("Fail :(" + error.responseText);
+  });
+};
+
+var describeStack = function() {
+  displayStatus("Requesting to describe VM..");
+  $request = $.get('/stacks');
+  $request.done(function(data) {
+    asif = data;
+    displayStatus(JSON.stringify(data));
+  });
+  $request.fail(function(error) {
+    asif = error;
    displayStatus("Fail :(" + error.responseText);
   });
 };
@@ -35,5 +79,6 @@ var deleteStack = function() {
 $(function() {
   $('#btnCreate').on('click', createStack);
   $('#btnDelete').on('click', deleteStack);
+  $('#btnGetInfo').on('click', describeStack);
 });
 
